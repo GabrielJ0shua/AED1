bf07dd244ee9a60473612a682cef77d5aa7cc5d7
Gabriel Joshua <gjoshuacalixto@outlook.com>
2020-09-14 11:02:36 -0300

    Lista com cabeçalho ordenada

---
+++ 1/exe1.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "tad1.h"
+
+int main(void){
+    int i,j;
+    lista *l;
+do
+{
+    printf("\n[1]Criar lista\n[2]Inserir elemento\n[3]Remover elemento\n[4]Imprimir a posição do número\n[5]Imprimir a lista\n[6]Liberar lista\n[7]Sair\n");
+    scanf("%d",&i);
+    switch (i)
+    {
+        case (1):
+            l = cria();
+            printf("\npronto\n");
+        break;
+
+        case (2):
+            printf("\nQual o número que deseja inserir? ");
+            scanf("%d",&j);
+            ord(l,j);
+        break;
+
+        case (3):
+            printf("\nQual o número que deseja apagar? ");
+            scanf("%d",&j);
+            rem(l,j);
+        break;
+
+        case (4):
+            printf("\nQual o número que deseja consultar? ");
+            scanf("%d",&j);
+            pos(l,j);
+        break;
+
+        case (5):
+            imprime(l);
+        break;
+
+        case (6):
+            libera(l);
+            l = cria();
+        break;
+
+        case (7):
+            libera(l);
+            return 0;
+        break;
+    }
+} while (i>0 && i<8);
+return 1;
+} 
---
+++ 1/tad1.h
@@ -0,0 +1,17 @@
+struct numero
+{
+    int nu;
+};
+struct caixa
+{
+    struct caixa *no;
+    struct numero num;
+};
+typedef struct caixa cad;
+typedef struct caixa* lista;
+lista *cria(void);
+void libera (lista *li);
+int ord(lista *li,int l);
+int rem(lista *li,int b);
+int pos(lista *li,int l);
+int imprime(lista *li);   
